f7a5cfe9e49c25a01a58f76e885b10a50add6de1
Mostafa Shaer <mostafa-alshaer@hotmail.com>
2020-07-18 19:44:24 +0300

    adding some logs messages to the routers

--- src/routers/task.js
+++ src/routers/task.js
@@ -1,62 +1,78 @@
 const express = require('express')
 const taskService = require('../services/task')
 const auth = require('../middleware/auth')
+const loggerFactory = require('../utils/loggerFactory')
 const router = new express.Router()
+const logger = loggerFactory.getLogger('routers/user')
 
 router.post('/tasks', auth, async (req, res)=> {
     try{
+        logger.info('creating a new task...')
         const task = await taskService.create(req.body, req.user._id)
         res.status(201).send(task)
     } catch(error){
+        logger.error('error while creating a new task, the error: ' + error)
         res.status(400).send(error)
     }
 })
 
 router.get('/tasks', auth, async (req, res)=> {
     try{
+        logger.info('getting a list of tasks...')
         const tasks = await taskService.getList(req.query, req.user._id)
         res.send(tasks)
     } catch(error){
+        logger.error('error while getting a list of tasks, the error: ' + error)
         res.status(500).send()
     }
 })
 
 router.get('/tasks/:id', auth, async (req, res)=> {
     try{
+        logger.info('getting a task by its id...')
         const task = await taskService.getByID(req.params.id, req.user.id)
         if (!task){
+            logger.debug('not found a task with an id equal to: ' + req.params.id)
             return res.status(404).send()
         }
         res.send(task)
     } catch(error){
+        logger.error('error while getting a task by its id, the error: ' + error)
         res.status(500).send()
     }
 })
 
 router.patch('/tasks/:id', auth, async (req, res)=> {
     try{
+        logger.info('updating a task info...')
         if(! await taskService.isValidUpdate(req.body) ){
+            logger.warn('a user tried to do an invalid update')
             return res.status(400).send({error: "Invalid updates"})
         }
         const task = await taskService.getByID(req.params.id, req.user.id)
         if (!task){
+            logger.debug('not found a task with an id equal to: ' + req.params.id)
             return res.status(404).send()
         }
         await taskService.update(task, req.body)
         res.send(task)
     } catch(error){
+        logger.error('error while updating a task, the error: ' + error)
         res.status(500).send(error)
     }
 })
 
 router.delete('/tasks/:id', auth, async (req, res)=> {
     try{
+        logger.info('deleting a task...')
         const task = await taskService.getByIdAndDelete(req.params.id, req.user.id)
         if (!task){
+            logger.debug('not found a task with an id equal to: ' + req.params.id)
             return res.status(404).send()
         }
         res.send(task)
     } catch(error){
+        logger.error('error while deleting a task, the error: ' + error)
         res.status(500).send()
     }
 })
--- src/routers/user.js
+++ src/routers/user.js
@@ -3,67 +3,83 @@ const userService = require('../services/user')
 const userAvatarService = require('../services/userAvatar')
 const auth = require('../middleware/auth')
 const multer = require('multer')
+const loggerFactory = require('../utils/loggerFactory')
 const router = new express.Router()
+const logger = loggerFactory.getLogger('routers/user')
 
 router.post('/users', async (req, res)=> {
     try {
+        logger.info('creating a new user...')
         const user = await userService.create(req.body)
         const token = await userService.generateAuthToken(user)
         res.status(201).send({user, token})
     } catch (error){
+        logger.error('error while creating a new user, the error: ' + error)
         res.status(400).send(error)
     }
 })
 
 router.post('/users/login', async (req, res)=> {
     try{
+        logger.debug('logging in a user, user email is: ' + req.body.email)
         const user = await userService.getByCredentials(req.body.email, req.body.password)
         const token = await userService.generateAuthToken(user)
         res.send({ user, token})
     } catch(error){
+        logger.error('error while logging in a user, the error: ' + error)
         res.status(400).send()
     }
 })
 
 router.post('/users/logout', auth, async (req, res)=> {
     try{
+        logger.debug('logging out a user, user email is: ' + req.user.email)
         await userService.logout(req.user, req.token)
         res.send()
     } catch(error){
+        logger.error('error while logging out a user, the error: ' + error)
         res.status(500).send()
     }
 })
 
 router.post('/users/logoutAll', auth, async (req, res)=> {
     try{
+        logger.debug('logging out a user from all places, user email is: ' + req.user.email)
         await userService.logoutAll(req.user)
         res.send()
     } catch(error){
+        logger.error('error while logging out a user from all places, the error: ' + error)
         res.status(500).send()
     }
 })
 
 router.get('/users/me', auth, async (req, res)=> {
+    logger.info('getting a user profile...')
     res.send(req.user)
 })
 
 router.patch('/users/me', auth, async (req, res)=> {
     try{
+        logger.info('updating a user info...')
         if(! await userService.isValidUpdate(req.body) ){
+            logger.warn('a user tried to do an invalid update')
             return res.status(400).send({error: "Invalid updates"})
         }
         await userService.update(req.user, req.body)
         res.send(req.user)
     } catch(error){
+        logger.error('error while updating a user info, the error: ' + error)
         res.status(500).send(error)
     }
 })
 
 router.delete('/users/me', auth, async (req, res)=> {
     try{
+        logger.info('deleting a user...')
         await userService.delete(req.user)
         res.send(req.user)
     } catch(error){
+        logger.error('error while deleting a user, the error: ' + error)
         res.status(500).send(error)
     }
 })
@@ -82,26 +98,37 @@ const upload = multer({
 })
 
 router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res)=> {
+    logger.info('uploading a user avatar...')
     await userAvatarService.upload(req.user, req.file)
     res.send()
 }, (error, req, res, next) => {
+    logger.error('error while uploading a user avatar, the error: ' + error)
     res.status(400).send({error : error.message})
 })
 
 router.delete('/users/me/avatar', auth, async (req, res)=> {
-    await userAvatarService.delete(req.user)
-    res.send()
+    try {
+        logger.info('deleting a user avatar...')
+        await userAvatarService.delete(req.user)
+        res.send()
+    } catch (error) {
+        logger.error('error while deleting a user avatar, the error: ' + error)
+        res.status(500).send()
+    }
 })
 
 router.get('/users/:id/avatar', async (req, res) => {
     try{
+        logger.info('getting a user avatar...')
         const avatar = await userAvatarService.get(req.params.id)
         if(!avatar){
+            logger.debug('not found avatar for the user with the id equal to ' + req.params.id)
             res.status(404).send()
         }
         res.set('Content-Type', 'image/png')
         res.send(avatar)
     } catch(error){
+        logger.error('error while getting a user avatar, the error: ' + error)
         res.status(500).send()
     }
 })
